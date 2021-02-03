c4618e0aae6573640672173ac61218198819c583
r3h6 <r3h6@outlook.com>
2021-02-03 22:25:28 +0100

    [TASK] Refactor authorization server factory

--- Classes/Controller/AuthorizationController.php
+++ Classes/Controller/AuthorizationController.php
@@ -2,8 +2,14 @@
 
 namespace R3H6\OidcServer\Controller;
 
+use League\OAuth2\Server\AuthorizationServer;
+use R3H6\Oauth2Server\Domain\Repository\UserRepository;
+
 
 class AuthorizationController extends \R3H6\Oauth2Server\Controller\AuthorizationController
 {
-
+    public function __construct(AuthorizationServer $oidcServer, UserRepository $userRepository)
+    {
+        parent::__construct($oidcServer, $userRepository);
+    }
 }
---
+++ Classes/Domain/Factory/AuthorizationServerFactory.php
@@ -0,0 +1,20 @@
+<?php
+namespace R3H6\OidcServer\Domain\Factory;
+
+use OpenIDConnectServer\IdTokenResponse;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use R3H6\Oauth2Server\Domain\Configuration;
+use League\OAuth2\Server\ResponseTypes\ResponseTypeInterface;
+
+class AuthorizationServerFactory extends \R3H6\Oauth2Server\Domain\Factory\AuthorizationServerFactory
+{
+    public function __invoke(Configuration $configuration)
+    {
+        return parent::__invoke($configuration);
+    }
+
+    protected function getResponseType(): ?ResponseTypeInterface
+    {
+        return GeneralUtility::makeInstance(IdTokenResponse::class);
+    }
+}
