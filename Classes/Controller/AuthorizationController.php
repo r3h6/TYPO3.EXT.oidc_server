9b031b86da4c31e10cf4c6f0fdc16c08607d4cea
r3h6 <r3h6@outlook.com>
2021-02-06 22:32:52 +0100

    [TASK] Update constructor signature

--- Classes/Controller/AuthorizationController.php
+++ Classes/Controller/AuthorizationController.php
@@ -4,12 +4,13 @@ namespace R3H6\OidcServer\Controller;
 
 use League\OAuth2\Server\AuthorizationServer;
 use R3H6\Oauth2Server\Domain\Repository\UserRepository;
+use R3H6\Oauth2Server\Domain\Repository\AccessTokenRepository;
 
 
 class AuthorizationController extends \R3H6\Oauth2Server\Controller\AuthorizationController
 {
-    public function __construct(AuthorizationServer $oidcServer, UserRepository $userRepository)
+    public function __construct(AuthorizationServer $oidcServer, UserRepository $userRepository, AccessTokenRepository $accessTokenRepository)
     {
-        parent::__construct($oidcServer, $userRepository);
+        parent::__construct($oidcServer, $userRepository, $accessTokenRepository);
     }
 }
